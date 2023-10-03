16ff33a1147dcdce032484b5ed013e5288f6247d
Baimanchick <baika132213@gmail.com>
2023-10-03 18:57:46 +0600

    aliodhoahdoaugd

--- src/components/CarsList.jsx
+++ src/components/CarsList.jsx
@@ -1,68 +1,68 @@
-import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchCars, selectCars } from "../store/slices/carsSlice";
-import { getImage } from "../functions/getImage";
-import { useNavigate } from "react-router-dom";
-import Detail from "../pages/Detail";
-import { Ring } from "@uiball/loaders";
-import "../css/car.css";
+// import React, { useEffect, useState } from "react";
+// import { useDispatch, useSelector } from "react-redux";
+// import { fetchCars, selectCars } from "../store/slices/carsSlice";
+// import { getImage } from "../functions/getImage";
+// import { useNavigate } from "react-router-dom";
+// import Detail from "../pages/Detail";
+// import { Ring } from "@uiball/loaders";
+// import "../css/car.css";
 
-function CarsList() {
-  const dispatch = useDispatch();
-  const cars = useSelector(selectCars);
-  const [carImages, setCarImages] = useState({});
-  const [isLoading, setIsLoading] = useState(true);
-  const navigate = useNavigate();
+// function CarsList() {
+//   const dispatch = useDispatch();
+//   const cars = useSelector(selectCars);
+//   const [carImages, setCarImages] = useState({});
+//   const [isLoading, setIsLoading] = useState(true);
+//   const navigate = useNavigate();
 
-  useEffect(() => {
-    setIsLoading(true);
-    dispatch(fetchCars());
-  }, []);
+//   useEffect(() => {
+//     setIsLoading(true);
+//     dispatch(fetchCars());
+//   }, []);
 
-  useEffect(() => {
-    const fetchCarImages = async () => {
-      const imageUrls = {};
-      for (const car of cars) {
-        const imageUrl = await getImage(car.id);
-        imageUrls[car.id] = imageUrl;
-      }
-      setCarImages(imageUrls);
-      setIsLoading(false);
-    };
+//   useEffect(() => {
+//     const fetchCarImages = async () => {
+//       const imageUrls = {};
+//       for (const car of cars) {
+//         const imageUrl = await getImage(car.id);
+//         imageUrls[car.id] = imageUrl;
+//       }
+//       setCarImages(imageUrls);
+//       setIsLoading(false);
+//     };
 
-    if (cars.length > 0) {
-      fetchCarImages();
-    }
-  }, [cars]);
+//     if (cars.length > 0) {
+//       fetchCarImages();
+//     }
+//   }, [cars]);
 
-  return (
-    <div>
-      <h1>Список электромашин:</h1>
-      {cars?.map((item) => (
-        <div key={item.id}>
-          <h2>{item.name}</h2>
-          <h3>{item.pathName}</h3>
-          <div>{item.buyPrice.value} сом</div>
+//   return (
+//     <div>
+//       <h1>Список электромашин:</h1>
+//       {cars?.map((item) => (
+//         <div key={item.id}>
+//           <h2>{item.name}</h2>
+//           <h3>{item.pathName}</h3>
+//           <div>{item.buyPrice.value} сом</div>
 
-          {isLoading ? (
-            <Ring size={40} lineWeight={5} speed={2} color="black" />
-          ) : (
-            <img
-              onClick={() => navigate(`/details/${item.id}`)}
-              src={carImages[item.id]}
-            />
-          )}
+// {isLoading ? (
+//   <Ring size={40} lineWeight={5} speed={2} color="black" />
+// ) : (
+//   <img
+//     onClick={() => navigate(`/details/${item.id}`)}
+//     src={carImages[item.id]}
+//   />
+// )}
 
-          <hr></hr>
-        </div>
-      ))}
+//           <hr></hr>
+//         </div>
+//       ))}
 
-      <a className="button" href="https://forms.amocrm.ru/rvldddc">
-        {" "}
-        Оставить
-      </a>
-    </div>
-  );
-}
+//       <a className="button" href="https://forms.amocrm.ru/rvldddc">
+//         {" "}
+//         Оставить
+//       </a>
+//     </div>
+//   );
+// }
 
-export default CarsList;
+// export default CarsList;
