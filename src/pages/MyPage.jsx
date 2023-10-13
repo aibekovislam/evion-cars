52c345821fb3cad2e2cfe65ce726466a32b936e1
Baimanchick <baika132213@gmail.com>
2023-10-13 17:59:57 +0600

    o;aiwhdoahwd

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import me from "../css/images/baimurat.jpg";
+import islam from "../css/images/islam.jpg";
 import "../css/my.scss";
 
 function MyPage() {
   return (
     <>
       <div className="my-container">
-        <h2>Этот сайт делал я </h2>
+        <h2>Этот сайт делали мы </h2>
         <img src={me} />
+        <img src={islam} />
       </div>
     </>
   );
